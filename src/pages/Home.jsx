0e272bbd50d9acacf089d036375f263b6cfd03f8
ashok770 <ashoktamata6002@gmail.com>
2026-07-03 18:07:42 +0530

    decided the background color and theme

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-green-400">0xAshok Portfolio</h1>
     </div>
   );
 };
